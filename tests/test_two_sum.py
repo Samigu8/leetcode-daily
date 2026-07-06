156e36506aedb004db96e9a364da42a2c1839c8e
Samuel Gutierrez <sag401@ensign.edu>
2026-07-06 14:38:19 -0600

    Refactor Two Sum implementation to use class-based structure and update tests accordingly

--- tests/test_two_sum.py
+++ tests/test_two_sum.py
@@ -1,4 +1,5 @@
-from solutions.two_sum import two_sum
+from solutions.two_sum import Solution, two_sum
 
 def test_two_sum_basic():
-    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+    solution = Solution()
+    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
